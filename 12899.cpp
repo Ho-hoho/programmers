7ce5a21882a1cf750fe501ff9afd96461e7e6d80
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2022-08-15 11:38:19 +0900

    Create 12899.cpp

---
+++ 12899.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    stack <int> s;
+    int a;
+    n-1;
+    while(n > 0){
+        s.push((n-1)%3);
+        n = (n-1)/3;
+    }
+    while(!s.empty()){
+        a = s.top();s.pop();
+        switch(a){
+            case 0 :
+                answer += "1";
+                break;
+            case 1 :
+                answer += "2";
+                break;
+            case 2 :
+                answer += "4";
+                break;
+        }
+    }
+    return answer;
+}
