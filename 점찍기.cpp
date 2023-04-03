b0aaad2260015cc527cd95fa18b0f9a9d998fd12
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2023-04-03 13:45:04 +0900

    Create 점찍기.cpp

---
+++ 점찍기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+long long solution(int _k, int _d) {
+    long long k = _k;
+    long long d = _d;
+    long long answer = 0;
+    long long ak = 0;
+    long long bk = d - (d % k);
+    while(ak <= d){
+        answer += (bk/k + 1);
+        ak += k;
+        while(ak * ak  + bk * bk > d * d && bk > 0){
+            bk -= k;
+        }
+        //bk = (long long)sqrt(d * d - ak * ak);
+    }
+    return answer;
+}
